e3cb20390c79f7a1e5890e7f6f9ec5358d758a85
Le Hoang Lam <lhlam2515@gmail.com>
2025-03-12 10:20:54 +0700

    MobileNav and NavLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -12,17 +10,6 @@ const Home = async () => {
       <h1 className="h1-bold text-primary-gradient">
         Welcome to the world of NextJS!
       </h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </div>
   );
 };
