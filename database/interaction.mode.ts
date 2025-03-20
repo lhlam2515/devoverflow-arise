3122b8e4570b648b57594c64a585e4df03abb3c8
Le Hoang Lam <lhlam2515@gmail.com>
2025-03-20 07:43:47 +0700

    Update types for all models

--- database/interaction.mode.ts
+++ database/interaction.mode.ts
@@ -1,4 +1,4 @@
-import { model, models, Schema, Types } from "mongoose";
+import { Schema, models, model, Types, Document } from "mongoose";
 
 export interface IInteraction {
   user: Types.ObjectId;
@@ -7,11 +7,12 @@ export interface IInteraction {
   actionType: "question" | "answer";
 }
 
+export interface IInteractionDoc extends IInteraction, Document {}
 const InteractionSchema = new Schema<IInteraction>(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    action: { type: String, required: true },
-    actionId: { type: Schema.Types.ObjectId, required: true },
+    action: { type: String, required: true }, // 'upvote', 'downvote', 'view', 'ask_question',
+    actionId: { type: Schema.Types.ObjectId, required: true }, // 'questionId', 'answerId',
     actionType: { type: String, enum: ["question", "answer"], required: true },
   },
   { timestamps: true }
