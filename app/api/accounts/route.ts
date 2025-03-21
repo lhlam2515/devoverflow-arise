ab720ddf094bc4286e8869b4f2955e9e64375a48
Le Hoang Lam <lhlam2515@gmail.com>
2025-03-21 21:56:06 +0700

    Implement Create & Read Account Routes

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
+import handlerError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const users = await Account.find();
+
+    return NextResponse.json({ success: true, data: users }, { status: 200 });
+  } catch (error) {
+    return handlerError(error, "api") as APIErrorResponse;
+  }
+}
+
+// Create Account
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validatedData = AccountSchema.parse(body);
+
+    const existingUser = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+    if (existingUser)
+      throw new ForbiddenError(
+        "An account with the same provider already exists"
+      );
+
+    const newAccount = await Account.create(validatedData);
+
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handlerError(error, "api") as APIErrorResponse;
+  }
+}
