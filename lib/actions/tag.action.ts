1dba7153fe882246d0f7a0b455837adc188d927b
Le Hoang Lam <lhlam2515@gmail.com>
2025-03-27 22:42:53 +0700

    Implement Tag Questions Action

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,16 +1,22 @@
 import { FilterQuery } from "mongoose";
 
+import { Question } from "@/database";
 import Tag from "@/database/tag.model";
+import { GetTagQuestionsParams } from "@/types/action";
 import {
   PaginatedSearchParams,
   ActionResponse,
   _Tag,
   ErrorResponse,
+  _Question,
 } from "@/types/global";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { PaginatedSearchParamsSchema } from "../validations";
+import {
+  GetTagQuestionsSchema,
+  PaginatedSearchParamsSchema,
+} from "../validations";
 
 export async function getTags(
   params: PaginatedSearchParams
@@ -72,6 +78,62 @@ export async function getTags(
       success: true,
       data: {
         tags: JSON.parse(JSON.stringify(tags)),
+        isNext,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getTagQuestions(
+  params: GetTagQuestionsParams
+): Promise<
+  ActionResponse<{ tag: _Tag; questions: _Question[]; isNext: boolean }>
+> {
+  const validationResult = await action({
+    params,
+    schema: GetTagQuestionsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { tagId, page = 1, pageSize = 10, query } = validationResult.params!;
+  const skip = (page - 1) * pageSize;
+  const limit = pageSize;
+
+  try {
+    const tag = await Tag.findById(tagId);
+    if (!tag) throw new Error("Tag not found");
+
+    const filterQuery: FilterQuery<typeof Question> = {
+      tags: { $in: [tagId] },
+    };
+
+    if (query) {
+      filterQuery.title = { $regex: query, $options: "i" };
+    }
+
+    const totalQuestions = await Question.countDocuments(filterQuery);
+
+    const questions = await Question.find(filterQuery)
+      .select("_id title views answers upvotes downvotes author createdAt")
+      .populate([
+        { path: "author", select: "name image" },
+        { path: "tags", select: "name" },
+      ])
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalQuestions > skip + questions.length;
+
+    return {
+      success: true,
+      data: {
+        tag: JSON.parse(JSON.stringify(tag)),
+        questions: JSON.parse(JSON.stringify(questions)),
         isNext,
       },
     };
