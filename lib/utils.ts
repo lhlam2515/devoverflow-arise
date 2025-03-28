e80bb60e9442845d3b93e8a631454acee89761a1
Le Hoang Lam <lhlam2515@gmail.com>
2025-03-28 20:27:15 +0700

    Implement Question Details Page

--- lib/utils.ts
+++ lib/utils.ts
@@ -47,3 +47,13 @@ export function getTimeStamp(createdAt: Date) {
   }
   return "just now";
 }
+
+export const formatNumber = (number: number) => {
+  if (number >= 1000000) {
+    return (number / 1000000).toFixed(1) + "M";
+  } else if (number >= 1000) {
+    return (number / 1000).toFixed(1) + "K";
+  } else {
+    return number.toString();
+  }
+};
