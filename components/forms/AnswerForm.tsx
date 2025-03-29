d216194afa15c95d0394b00c692b57f5e3ee086a
Le Hoang Lam <lhlam2515@gmail.com>
2025-03-29 18:20:45 +0700

    Implement Create Answer Form

---
+++ components/forms/AnswerForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { AnswerSchema } from "@/lib/validations";
+
+import { Button } from "../ui/button";
+
+const Editor = dynamic(() => import("@/components/editor"), {
+  ssr: false,
+});
+
+const AnswerForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isAISubmitting, setIsAISubmitting] = useState(false);
+
+  const editorRef = useRef<MDXEditorMethods>(null);
+
+  const form = useForm<z.infer<typeof AnswerSchema>>({
+    resolver: zodResolver(AnswerSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const handleSubmit = async (values: z.infer<typeof AnswerSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+        <h4 className="paragraph-semibold text-dark400-light800">
+          Write your answer here
+        </h4>
+        <Button
+          className="btn-primary! border-light700-dark400 text-primary-500 dark:text-primary-500 cursor-pointer gap-1.5 rounded-md px-4 py-2.5 shadow-none"
+          disabled={isAISubmitting}
+        >
+          {isAISubmitting ? (
+            <>
+              <ReloadIcon className="mr-2 size-4 animate-spin" />
+              Generating...
+            </>
+          ) : (
+            <>
+              <Image
+                src="/icons/stars.svg"
+                alt="Generate AI Answer"
+                width={12}
+                height={12}
+                className="object-contain"
+              />
+              Generate AI Answer
+            </>
+          )}
+        </Button>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="mt-6 flex w-full flex-col gap-10"
+        >
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem className="flex w-full flex-col gap-3">
+                <FormControl className="mt-3.5">
+                  <Editor
+                    value={field.value}
+                    editorRef={editorRef}
+                    fieldChange={field.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end">
+            <Button
+              type="submit"
+              className="bg-primary-gradient w-fit cursor-pointer"
+            >
+              {isSubmitting ? (
+                <>
+                  <ReloadIcon className="mr-2 size-4 animate-spin" />
+                  Posting...
+                </>
+              ) : (
+                "Post Answer"
+              )}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+export default AnswerForm;
