69011a29d67b9d5e7c5314835badacd742e4875b
Le Hoang Lam <lhlam2515@gmail.com>
2025-04-01 11:40:04 +0700

    Develop Collections Page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_COLLECTIONS } from "@/constants/states";
+import { getSavedQuestions } from "@/lib/actions/collection.action";
 
-const Collections = () => {
-  return <div>Collections</div>;
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Collections = async ({ searchParams }: SearchParams) => {
+  const { page = 1, pageSize = 1, query, filter } = await searchParams;
+
+  const { success, data, error } = await getSavedQuestions({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+    filter,
+  });
+
+  const { collection } = data || {};
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100-light900">Saved Questions</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route={ROUTES.COLLECTION}
+          imgSrc="/icons/search.svg"
+          placeholder="Search quesitons..."
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={collection}
+        empty={EMPTY_COLLECTIONS}
+        render={(collection) => (
+          <div className="mt-10 flex w-full flex-col gap-6">
+            {collection.map((item) => (
+              <QuestionCard key={item._id} question={item.quesiton} />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
 };
 
 export default Collections;
