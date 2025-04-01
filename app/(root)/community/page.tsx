6ebe4f93483fc7bf762985a2325269dd531df4df
Le Hoang Lam <lhlam2515@gmail.com>
2025-04-01 21:05:09 +0700

    Implement Display all Users

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import UserCard from "@/components/cards/UserCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_USERS } from "@/constants/states";
+import { getUsers } from "@/lib/actions/user.action";
+import { RouteParams } from "@/types/global";
 
-const Community = () => {
-  return <div>Community</div>;
+const Community = async ({ searchParams }: RouteParams) => {
+  const { page, pageSize, query, filter } = await searchParams;
+
+  const { success, data, error } = await getUsers({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+    filter,
+  });
+
+  const { users } = data || {};
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100-light900">All Users</h1>
+
+      <h1 className="mt-11">
+        <LocalSearch
+          route={ROUTES.COMMUNITY}
+          imgSrc="/icons/search.svg"
+          placeholder="There are some great devs here!"
+          otherClasses="flex-1"
+        />
+      </h1>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={users}
+        empty={EMPTY_USERS}
+        render={(users) => (
+          <div className="mt-12 flex flex-wrap gap-5">
+            {users?.map((user) => <UserCard key={user._id} {...user} />)}
+          </div>
+        )}
+      />
+    </>
+  );
 };
 
 export default Community;
