ba9d8f94d511c5a0a8545693bbb65e4078a7e1dd
Le Hoang Lam <lhlam2515@gmail.com>
2025-04-01 22:20:26 +0700

    Create Filter Component and Update HomeFilter

---
+++ components/filters/CommonFilter.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { formUrlQuery } from "@/lib/url";
+import { cn } from "@/lib/utils";
+
+interface Filter {
+  name: string;
+  value: string;
+}
+
+interface Props {
+  filters: Filter[];
+  otherClasses?: string;
+  containerClasses?: string;
+}
+
+const CommonFilter = ({
+  filters,
+  otherClasses = "",
+  containerClasses = "",
+}: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const paramsFilter = searchParams.get("filter");
+
+  const handleUpdateParams = (value: string) => {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: "filter",
+      value,
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
+
+  return (
+    <div className={cn("relative", containerClasses)}>
+      <Select
+        onValueChange={handleUpdateParams}
+        defaultValue={paramsFilter || undefined}
+      >
+        <SelectTrigger
+          className={cn(
+            "body-normal! no-focus border-light800-dark300! bg-light800-dark300! text-dark500-light700! border px-5 py-2.5",
+            otherClasses
+          )}
+          aria-label="Filter options"
+        >
+          <div className="line-clamp-1 flex-1 text-left">
+            <SelectValue placeholder="Select a filter" />
+          </div>
+        </SelectTrigger>
+
+        <SelectContent>
+          <SelectGroup>
+            {filters.map((filter) => (
+              <SelectItem key={filter.value} value={filter.value}>
+                {filter.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default CommonFilter;
--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -1,58 +1,46 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useState } from "react";
 
+import { HomePageFilters } from "@/constants/filters";
 import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
 import { cn } from "@/lib/utils";
 
 import { Button } from "../ui/button";
 
-const filters = [
-  { name: "React", value: "react" },
-  { name: "Next.js", value: "next.js" },
-  // { name: "Newest", value: "newest" },
-  // { name: "Popular", value: "popular" },
-  // { name: "Unanswered", value: "unanswered" },
-  // { name: "Recommended", value: "recommended" },
-];
-
 const HomeFilter = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const filterParams = searchParams.get("filter");
-  const [active, setActive] = useState("");
+
+  const paramsFilter = searchParams.get("filter");
 
   const handleTypeClick = (filter: string) => {
     let newUrl = "";
 
-    if (filter === active) {
-      setActive("");
-
+    if (filter === paramsFilter) {
       newUrl = removeKeysFromUrlQuery({
         params: searchParams.toString(),
         keysToRemove: ["filter"],
       });
     } else {
-      setActive(filter);
-
       newUrl = formUrlQuery({
         params: searchParams.toString(),
         key: "filter",
         value: filter,
       });
     }
+
     router.push(newUrl, { scroll: false });
   };
 
   return (
     <div className="mt-10 hidden flex-wrap gap-3 sm:flex">
-      {filters.map((filter) => (
+      {HomePageFilters.map((filter) => (
         <Button
           key={filter.name}
           className={cn(
             `body-medium rounded-lg px-6 py-3 capitalize shadow-none`,
-            active === filter.value
+            paramsFilter === filter.value
               ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
               : "bg-light-800 dark:bg-dark-400 text-dark-500 dark:text-light-500 hover:bg-light-800 dark:hover:bg-dark-300"
           )}
